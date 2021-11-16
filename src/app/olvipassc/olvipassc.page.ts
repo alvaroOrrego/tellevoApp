572edb7fc178c2efb58941c02973547ab6d672a1
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2021-11-16 10:49:00 -0300

    actualizar contraseña

--- src/app/olvipassc/olvipassc.page.ts
+++ src/app/olvipassc/olvipassc.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Animation, AnimationController, AlertController} from '@ionic/angular';
+import { Animation, AnimationController, AlertController } from '@ionic/angular';
 import { CrudService } from '../crud.service';
 import { Router } from '@angular/router';
 
@@ -12,6 +12,10 @@ import { Router } from '@angular/router';
 export class OlvipasscPage implements OnInit {
 
   rut = "";
+  nombres = "";
+  apellidos: any;
+  ocupacion: any;
+  email: any;
   fono = "";
 
   constructor(private animationCtrl: AnimationController, public alerta: AlertController,
@@ -20,63 +24,102 @@ export class OlvipasscPage implements OnInit {
   ngOnInit() {
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     const animation = this.animationCtrl
-  .create()
-  .addElement(document.querySelector('#imgChofer'))
-  .duration(1500)
-  .iterations(1)
-  .keyframes([
-    { offset: 0, transform: 'scale(1)', opacity: '1' },
-    { offset: 0.5, transform: 'scale(1.3)', opacity: '0.3' },
-    { offset: 1, transform: 'scale(1)', opacity: '1' }
-  ]);
+      .create()
+      .addElement(document.querySelector('#imgChofer'))
+      .duration(1500)
+      .iterations(1)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.3)', opacity: '0.3' },
+        { offset: 1, transform: 'scale(1)', opacity: '1' }
+      ]);
 
-  animation.play();
+    animation.play();
 
   }
 
-  async actualizarPass(txtRut:HTMLInputElement, txtFono:HTMLInputElement)
-  {
-    if(txtRut.value.trim().length === 0 || txtFono.value.trim().length === 0){
+  async actualizarPass(txtRut: HTMLInputElement, txtNombres: HTMLInputElement,
+    txtApellidos: HTMLInputElement, txtOcupacion: HTMLInputElement,
+    txtEmail: HTMLInputElement, txtFono: HTMLInputElement) {
 
-      console.log("entro");
-  
-      this.router.navigateByUrl('/loginconductor');
-
-    }else{
-      console.log("ola");
+    this.router.navigateByUrl('/loginconductor');
+    this,this.alertaCambioContra();
 
     const valor = await this.crud.buscar(txtRut.value);
-    if (valor != null){
-      this.rut = valor[0].txtRut;
-      this.fono = valor[0].txtFono;
+    if (valor != null) {
+      this.rut = valor[0].rut;
+      this.nombres = valor[0].nombres;
+      this.apellidos = valor[0].apellidos;
+      this.ocupacion = valor[0].ocupacion;
+      this.email = valor[0].email;
+      this.fono = valor[0].fono;
 
 
-    if (txtRut.value.trim().length != 0)
-    {
-      this.rut  = txtRut.value;
-    }
-
-    if (txtFono.value.trim().length != 0)
-    {
-      this.fono = txtFono.value;
-    }
-
-    const datos = [{
-                    "rut": txtRut.value,
-                    "fono": txtFono.value
-
-    }];
-    await this.crud.agregarConKey(txtRut.value,datos);
-        txtRut.value = "";
-        txtFono.value = "";
-
-        }
-        else{ console.log("ta loco tio");
-
-        }
+      if (txtRut.value.trim().length != 0) {
+        this.rut = txtRut.value;
       }
-    }
 
+     /*  if (txtNombres.value.trim().length != 0) {
+        this.nombres = txtNombres.value;
+      }
+
+      if (txtApellidos.value.trim().length != 0) {
+        this.apellidos = txtApellidos.value;
+      }
+
+      if (txtOcupacion.value.trim().length != 0) {
+        this.ocupacion = txtOcupacion.value;
+      }
+
+      if (txtEmail.value.trim().length != 0) {
+        this.email = txtEmail.value;
+      } */
+
+      if (txtFono.value.trim().length != 0) {
+        this.fono = txtFono.value;
+      }
+
+      const datos = [{
+        "rut": this.rut,
+        "nombres": this.nombres,
+        "apellidos": this.apellidos,
+        "ocupacion": this.ocupacion,
+        "email": this.email,
+        "fono": this.fono
+
+      }];
+      await this.crud.agregarConKey(txtRut.value, datos);
+      txtRut.value = "";
+      txtNombres.value = "";
+      txtApellidos.value = "";
+      txtOcupacion.value = "";
+      txtEmail.value = "";
+      txtFono.value = "";
+
+    }
+    else {
+      console.log("ta loco tio");
+
+    }
+  }
+
+
+
+  async alertaCambioContra(){
+    const alert = await this.alerta.create({
+      cssClass:'my-custom-class',
+      header:'¡Contraseña Actualizada!',
+      message: '¡Contraseña actualizada correctamente, por favor verifique su correo para confirmar el cambio!',
+      buttons: [
+        {
+          text:'OK',
+
+            }
+        ]
+      });
+
+      await alert.present();
+  }
 }
